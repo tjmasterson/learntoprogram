79ee8f36031ca5103b4ae316059c7a74f7e66166
Taylor Masterson <taylor.j.masterson@gmail.com>
2014-03-22 15:51:10 -0500

    Added 99 bottles exercise

---
+++ Chapter_7_99bottles.rb
@@ -0,0 +1,11 @@
+bottles = 99
+while bottles >= 2
+puts bottles.to_s + " bottles of beer on the wall"
+puts bottles.to_s + " bottles of beer take one down pass it around"
+
+bottles = bottles - 1
+end
+
+puts "1 bottle of beer on the wall"
+puts "1 bottle of beer take it down pass it around"
+puts "no bottles of beer on the wall"
