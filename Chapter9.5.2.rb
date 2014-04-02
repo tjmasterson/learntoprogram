4c29c38ee024ab00f65cab53a95d07eca5cfbb9d
Taylor Masterson <taylor.j.masterson@gmail.com>
2014-04-01 20:20:05 -0500

    Chapter 9

--- Chapter9.5.2.rb
+++ Chapter9.5.2.rb
@@ -7,17 +7,8 @@ roman = roman + 'C' * (number % 500 / 100)
 roman = roman + 'L' * (number % 100 / 50)
 roman = roman + 'X' * (number % 50 / 10)
 roman = roman + 'V' * (number % 10 / 5)
-
-#numlength = number.to_s.length
-#lastdigit = number.to_s
-
-numberstring = number.to_s
-lastdigit = numberstring[numberstring.length-1, numberstring.length-1]
-if lastdigit == '9'
-	roman = roman + 'IV' 
-else
-    roman = roman + 'I' * (number % 5 / 1)
-end
+roman = roman + 'I' * (number % 5 / 1)
+roman
 
 end
 
--- Chapter9.5.3.rb
+++ Chapter9.5.3.rb
@@ -7,9 +7,19 @@ roman = roman + 'C' * (number % 500 / 100)
 roman = roman + 'L' * (number % 100 / 50)
 roman = roman + 'X' * (number % 50 / 10)
 roman = roman + 'V' * (number % 10 / 5)
-roman = roman + 'I' * (number % 5 / 1)
-roman
+
+#numlength = number.to_s.length
+#lastdigit = number.to_s
+
+numberstring = number.to_s
+lastdigit = numberstring[numberstring.length-1, numberstring.length-1]
+
+if lastdigit == '9'
+	roman = roman + 'IV' 
+else
+    roman = roman + 'I' * (number % 5 / 1)
+end
 
 end
 
-puts (roman(2987))
+puts (roman(1989))
